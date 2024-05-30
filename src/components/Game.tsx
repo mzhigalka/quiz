88b622dfceb686882cfb159807155c2044b016c8
Matvey Zhigalka <mzhigalka@gmail.com>
2024-05-30 12:15:02 +0300

    added TypeScript

---
+++ src/components/Game.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import { questions } from '../App';
+
+interface GameProps {
+  step: number;
+  question: {
+    title: string
+    variants: string[];
+    correct: number;
+  };
+  onClickVariant: (index: number) => void;
+}
+
+const Game: FC<GameProps> = ({ step, question, onClickVariant }) => {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text: string, index: number) => (
+          <li
+            onClick={() => {
+              onClickVariant(index);
+            }}
+            key={text}
+          >
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Game
---
+++ src/components/Result.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { questions } from "../App";
+
+interface ResultProp {
+  correct: number;
+}
+
+const Result: FC<ResultProp> = ({ correct }) => {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+export default Result;
