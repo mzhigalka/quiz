c6bde963b1a42fcd0a3a36caa687738a9a9ad882
Matvey Zhigalka <mzhigalka@gmail.com>
2024-05-30 12:25:42 +0300

    update again

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,50 @@
 import "./scss/index.scss";
 import React from "react";
 
-const questions = [
+import Result from "./components/Result";
+import Game from "./components/Game";
+
+export const questions = [
   {
-    title: "React - это ... ?",
-    variants: ["библиотека", "фреймворк", "приложение"],
-    correct: 0,
+    "title": "React - это ... ?",
+    "variants": ["библиотека", "фреймворк", "приложение"],
+    "correct": 0,
   },
   {
-    title: "Компонент - это ... ",
-    variants: [
+    "title": "Компонент - это ... ",
+    "variants": [
       "приложение",
       "часть приложения или страницы",
       "то, что я не знаю что такое",
     ],
-    correct: 1,
+    "correct": 1,
   },
   {
-    title: "Что такое JSX?",
-    variants: [
+    "title": "Что такое JSX?",
+    "variants": [
       "Это простой HTML",
       "Это функция",
       "Это тот же HTML, но с возможностью выполнять JS-код",
     ],
-    correct: 2,
+    "correct": 2,
   },
   {
-    title: "Что такое useState?",
-    variants: [
+    "title": "Что такое useState?",
+    "variants": [
       "Это hook который отслеживает состояния",
       "Это переменная",
       "Это функция",
     ],
-    correct: 0,
+    "correct": 0,
   },
 ];
 
-function Result({ correct }) {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>
-        Вы отгадали {correct} ответа из {questions.length}
-      </h2>
-      <a href="/">
-        <button>Попробовать снова</button>
-      </a>
-    </div>
-  );
-}
-
-function Game({ step, question, onClickVariant }) {
-  const percentage = Math.round((step / questions.length) * 100);
-
-  return (
-    <>
-      <div className="progress">
-        <div
-          style={{ width: `${percentage}%` }}
-          className="progress__inner"
-        ></div>
-      </div>
-      <h1>{question.title}</h1>
-      <ul>
-        {question.variants.map((text, index) => (
-          <li
-            onClick={() => {
-              onClickVariant(index);
-            }}
-            key={text}
-          >
-            {text}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
-
 function App() {
-  const [step, setStep] = React.useState(0);
-  const [correct, setCorrect] = React.useState(0);
+  const [step, setStep] = React.useState<number>(0);
+  const [correct, setCorrect] = React.useState<number>(0);
   const question = questions[step];
 
-  const onClickVariant = (index) => {
+  const onClickVariant = (index: number) => {
     setStep(step + 1);
 
     if (index === question.correct) {
